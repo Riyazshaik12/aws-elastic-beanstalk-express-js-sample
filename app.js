04fc6fe9d0fe2b255b099cb91629c60711768edb
Riyazshaik12 <160094146+Riyazshaik12@users.noreply.github.com>
2024-02-16 15:29:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hlo World!'));
+app.get('/', (req, res) => res.send('Team 9 hellllllooooo Hlo World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
